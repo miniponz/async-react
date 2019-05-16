115eae5308d01dc935168ffcf6c592fdd067bbd0
Cara Ponzini <ponzini@gmail.com>
2019-05-16 14:36:44 -0700

    tests and api call

---
+++ src/components/Characters.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Character from './Character';
+
+function Characters({ characters }) {
+  const charList = characters.map((character) => (
+    <li key={character.name}>
+      <Character name={character.name} status={character.status} species={character.species} image={character.image} />
+    </li>
+  ));
+
+  return (
+    <ul>
+      {charList}
+    </ul>
+  );
+}
+
+Characters.propTypes = {
+  characters: PropTypes.arrayOf(PropTypes.object).isRequired
+};
+
+export default Characters;
