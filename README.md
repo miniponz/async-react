752c175914aee265c798acc813e5959cfd8955b3
Cara Ponzini <ponzini@gmail.com>
2019-05-16 11:56:02 -0700

    test for quote pass

---
+++ README.md
@@ -0,0 +1,6 @@
+//Make a service
+//Make a container
+//check updates
+//testing for quotes = snapshot, snapshot button click test, 
+//container snapshot test
+//last one snapshot and button click test.
--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,5 +1,21 @@
-function tree(){
-  return 'tree';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Quote({ character, quote, image }) {
+  return (
+    <section>
+      <h3>{character}</h3>
+      <img src={image}></img>
+      <p>{quote}</p>
+    </section>
+  );
 }
 
-tree();
+Quote.propTypes = {
+  character: PropTypes.string.isRequired,
+  quote: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired
+};
+
+export default Quote;
+
